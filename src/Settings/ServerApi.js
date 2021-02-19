3e539437069775a4def413a74563567eaeeb4dbe
pedro742k2 <pmpb742k2@gmail.com>
2021-02-19 23:42:04 +0000

    Changed local to heroku server

--- src/Settings/ServerApi.js
+++ src/Settings/ServerApi.js
@@ -1,4 +1,4 @@
-// const ServerApi = "https://bee-project-server.herokuapp.com";
-const ServerApi = "http://localhost:5000";
+const ServerApi = "https://bee-project-server.herokuapp.com";
+// const ServerApi = "http://localhost:5000";
 
 export default ServerApi;
